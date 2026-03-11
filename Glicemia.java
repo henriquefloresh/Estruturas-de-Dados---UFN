aa4c287659a22d08c585c702b0fedc3ff1cd0400
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-03-11 09:51:28 -0300

    Create Glicemia.java

---
+++ Glicemia.java
@@ -0,0 +1,11 @@
+public class Glicemia {
+    int valor;
+    String data;
+    String hora;
+
+    public Glicemia(int valor, String data, String hora){
+        this.valor = valor;
+        this.hora = hora;
+        this.data = data;
+    }
+}
