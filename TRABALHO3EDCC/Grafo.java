a8566a2f47f0df19367eb250771d34b0ec07e4b3
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-06-10 13:25:31 -0300

    Add files via upload

---
+++ TRABALHO3EDCC/Grafo.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+public class Grafo {
+    int matrizADJ[][];
+    int qtdVertices;
+    ArrayList<String> vertices;
+
+    public Grafo(ArrayList<String> vertices) {
+        this.vertices = new ArrayList<>();
+        this.vertices.addAll(vertices);
+        this.qtdVertices = vertices.size();
+        this.matrizADJ = new int[this.qtdVertices][this.qtdVertices];
+
+        //inicializar com 0 a matriz que representa Grafo
+        for(int i = 0; i < qtdVertices; i++) {
+            for(int j = 0; j < qtdVertices; j++){
+                this.matrizADJ[i][j] = 0;
+            }
+        }
+    }
+
+    void mostrarMatriz(){
+        for(String v : this.vertices){
+            System.out.print("    " + v);
+        }
+        System.out.println();
+
+        for(int i = 0; i < qtdVertices; i++) {
+            System.out.print(this.vertices.get(i) + "   ");
+            for(int j = 0; j < qtdVertices; j++){
+                System.out.print(matrizADJ[i][j] + "    ");
+            }
+            System.out.println();
+        }
+    }
+
+    void mostrarGrafo(){
+        for (int i = 0; i < this.qtdVertices; i++){
+            System.out.print(this.vertices.get(i) + ": ");
+            for(int j = 0; j < this.qtdVertices; j++){
+                if(matrizADJ[i][j] != 0) {
+                    System.out.print(this.vertices.get(j) + "   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    void inserirAresta(int origem, int destino){
+        if (this.matrizADJ[origem][destino] == 0){
+            this.matrizADJ[origem][destino] = 1;
+        }
+    }
+    
+    //retorna o indice na lista de vertices de um vertice
+    int pegarIndice(String vertice){
+        return this.vertices.indexOf(vertice);
+    }
+}
